69d5bfdbdf376a248166a542a5584c869f694945
Ethan Bailie <ethanbailie@hotmail.com>
2023-12-19 10:04:37 -0500

    added data orchestration using mage

---
+++ mage/data_exporters/update_raws.py
@@ -0,0 +1,21 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.postgres import Postgres
+from pandas import DataFrame
+from os import path
+
+from mage.utils.fetcher import paperFetcher as pf
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_postgres(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a PostgreSQL database.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#postgresql
+    """
+    pf().write_to_db(df=df)
---
+++ mage/data_loaders/paper_fetcher.py
@@ -0,0 +1,30 @@
+import io
+import pandas as pd
+import requests
+from mage.utils.fetcher import paperFetcher as pf
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    papers_json = pf().get_papers_json()
+    if papers_json == '[]':
+        return None
+
+    papers_df = pd.read_json(papers_json)
+    return papers_df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
